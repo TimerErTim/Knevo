0cee715656fa2c78f0c8a411b5debe5db07ab975
TimerErTim <timerertim@gmail.com>
2021-10-18 12:48:19 +0200

    Improve XOR example
    Add save and result feature and change outputActivations to step

--- src/test/kotlin/eu/timerertim/knevo/instinct/xor/InstinctXOR.kt
+++ src/test/kotlin/eu/timerertim/knevo/instinct/xor/InstinctXOR.kt
@@ -3,11 +3,13 @@ package eu.timerertim.knevo.instinct.xor
 import eu.timerertim.knevo.activation.Relu
 import eu.timerertim.knevo.activation.Sigmoid
 import eu.timerertim.knevo.activation.Sign
+import eu.timerertim.knevo.activation.Step
 import eu.timerertim.knevo.activation.Tanh
 import eu.timerertim.knevo.instinct.InstinctInstanceBuilder
 import eu.timerertim.knevo.instinct.InstinctPoolBuilder
 import eu.timerertim.knevo.instinct.globalInstinctInstance
 import eu.timerertim.knevo.shared.xor.XOREnvironment
+import java.io.File
 
 fun main() {
     val environment = XOREnvironment()
@@ -17,6 +19,7 @@ fun main() {
         .mutateAddRecurrentConnectionChance(0F)
         .mutateRemoveConnectionChance(2.15F)
         .hiddenActivations(listOf(Tanh(), Sigmoid(), Relu(), Sign()))
+        .outputActivations(Step())
         .build()
 
     val pool = InstinctPoolBuilder()
@@ -31,5 +34,16 @@ fun main() {
         println("Top Fitness : " + pool.topFitness)
         println("Generation : ${pool.generation}")
 
-    } while (pool.topFitness < 0.99)
+    } while (pool.topFitness < 1)
+
+    val network = pool.topGenome
+
+    println("------------")
+    println("0 | 0 = ${network(floatArrayOf(0F, 0F)).first()}")
+    println("0 | 1 = ${network(floatArrayOf(0F, 1F)).first()}")
+    println("1 | 0 = ${network(floatArrayOf(1F, 0F)).first()}")
+    println("1 | 1 = ${network(floatArrayOf(1F, 1F)).first()}")
+    println("------------")
+
+    pool.topGenome.save("out${File.separator}instinctXOR_fully_trained.knv")
 }

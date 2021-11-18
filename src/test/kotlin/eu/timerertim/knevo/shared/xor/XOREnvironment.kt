da37e80a9ba81166435b40220edb2b4388c63918
TimerErTim <timerertim@gmail.com>
2021-11-18 18:35:22 +0100

    Implement XOREnvironment using FitnessFunction

--- src/test/kotlin/eu/timerertim/knevo/shared/xor/XOREnvironment.kt
+++ src/test/kotlin/eu/timerertim/knevo/shared/xor/XOREnvironment.kt
@@ -1,21 +1,19 @@
 package eu.timerertim.knevo.shared.xor
 
-import eu.timerertim.knevo.Environment
+import eu.timerertim.knevo.FitnessFunction
 import eu.timerertim.knevo.Genome
 
-class XOREnvironment : Environment<Genome> {
-    override fun evaluateFitness(population: List<Genome>) {
-        for (gene in population) {
-            var fitness = 0f
-            for (i in 0..1)
-                for (j in 0..1) {
-                    val inputs = floatArrayOf(i.toFloat(), j.toFloat())
-                    val output = gene(inputs)
-                    gene.doReset = true
-                    val expected = i xor j
-                    fitness += (expected - output[0]) * (expected - output[0])
-                }
-            gene.fitness = 1 - fitness
-        }
+class XOREnvironment : FitnessFunction<Genome> {
+    override suspend fun invoke(genome: Genome): Float {
+        var fitness = 0F
+        for (i in 0..1)
+            for (j in 0..1) {
+                genome.doReset = true
+                val inputs = floatArrayOf(i.toFloat(), j.toFloat())
+                val output = genome(inputs)
+                val expected = i xor j
+                fitness += (expected - output[0]) * (expected - output[0])
+            }
+        return 1 - fitness
     }
 }
